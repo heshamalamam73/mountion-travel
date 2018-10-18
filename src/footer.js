1e9b95c4e5b7f445152fed630a57e413507357c4
mahmoud hassan <mahmoudhassan@momuzio.local>
2018-10-19 01:17:57 +0200

    initial commit

--- src/footer.js
+++ src/footer.js
@@ -9,7 +9,7 @@ class Footer  extends Component {
                
                       <footer>
                         <p>Images courtesy of <a href="https://www.pexels.com/">pexels</a>.</p>
-                        <p>created by / momuzio 2018</p>
+                        <p>created by / momuzio 9/2018</p>
                         <ul>
                         <li><a href="/"><i className="fa fa-twitter-square fa-2x"></i></a></li>
                         <li><a href="/"><i className="fa fa-facebook-square fa-2x"></i></a></li>
